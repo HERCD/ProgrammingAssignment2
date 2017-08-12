8536a664a30e34b55c343e73638e4b01c64846c1
HERCD <daan.herckenrath@gmail.com>
2017-08-12 18:15:40 +1000

    edit cachematrix.R and add cachevector.R

---
+++ cachevector.R
@@ -0,0 +1,29 @@
+##makeVector creates a special "vector", with a list of properties
+##which can be use to get and set the mean to store in cache
+
+makeVector <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
+}
+
+##Get mean of vector created by makeVector
+cachemean <- function(x, ...) {
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
+}
